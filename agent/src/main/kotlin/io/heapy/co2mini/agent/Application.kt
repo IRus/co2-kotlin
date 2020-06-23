6e51c7f5b4177af910ed18ec4835e264252428c5
Ruslan Ibragimov <ruslan@ibragimov.by>
2020-06-23 15:21:08 +0300

    Read bytes from usb interface

--- agent/src/main/kotlin/io/heapy/co2mini/agent/Application.kt
+++ agent/src/main/kotlin/io/heapy/co2mini/agent/Application.kt
@@ -3,6 +3,7 @@ package io.heapy.co2mini.agent
 import com.typesafe.config.ConfigFactory
 import io.github.config4k.extract
 import javax.usb.UsbDevice
+import javax.usb.UsbEndpoint
 import javax.usb.UsbHostManager
 import javax.usb.UsbHub
 
@@ -13,7 +14,23 @@ fun main() {
     val device = findDevice(usbServices.rootUsbHub, config.device)
 
     device?.let {
-        println(it)
+        val configuration = device.activeUsbConfiguration
+        val iface = configuration.getUsbInterface(0)
+        iface.claim()
+        try {
+            val endpoint: UsbEndpoint = iface.getUsbEndpoint(0x83.toByte())
+            val pipe = endpoint.usbPipe
+            pipe.open()
+            try {
+                val data = ByteArray(8)
+                val received = pipe.syncSubmit(data)
+                println("$received bytes received")
+            } finally {
+                pipe.close()
+            }
+        } finally {
+            iface.release()
+        }
     }
 }
 
